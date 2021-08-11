1890c89fe6f8545ee55b5863c2701c77c948a03b
Mike Torres <miketorres@Mikes-MacBook-Pro.local>
2021-08-10 17:17:33 -0700

    Initial Commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Let's Build Blue360 Media App</Text>
       <StatusBar style="auto" />
     </View>
   );
